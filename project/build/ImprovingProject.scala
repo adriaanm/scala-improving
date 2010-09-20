3c4fb1c5fcc4b6ea8c9359895b2505336404fb66
Paul Phillips <paulp@improving.org>
2010-09-20 14:15:24 -0700

    Took my own advice about not making the whole world enjoy the publishing dependencies.  Now there are no plugins in the main project, and publishing is done from the publish branch, via the git processor at   http://github.com/paulp/git-sbt-processor which does a little presto-changeo.

--- project/build/ImprovingProject.scala
+++ project/build/ImprovingProject.scala
@@ -2,25 +2,12 @@ import sbt._
 
 class ImprovingProject(info: ProjectInfo)
         extends DefaultProject(info)
-           with net.ps.GithubPlugin {  
+           with ImprovingPublish {
 
   // repos
   val localMaven    = "Maven" at "file://"+Path.userHome+"/.m2/repository"
   val localIvy      = "Ivy" at "file://"+Path.userHome+"/.ivy2/local"
-  val pluginRepo    = githubRepo("siasia/plugin")
-  val uploaderRepo  = githubRepo("siasia/uploader")
-  val improvingRepo = githubRepo("paulp/scala-improving")
-
-  // dependencies
-  val uploader = "net.ps" % "github-uploader_2.7.7" % "1.0"
-  
-  // register credentials
-  net.ps.github.Credentials(Path.userHome / ".github" / "credentials")
-  
-  // local use
-  // override def localScala =
-  //   defineScala("2.8.0-local", new java.io.File("/scala/trunk/target/pack")) :: Nil
-  
-  override val publishTo = Some(improvingRepo)
-  override val publishToGithub = true
 }
+
+// a dummy trait on the main branch, see the publish branch for implementation.
+trait ImprovingPublish
--- project/plugins/Plugins.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-
-class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val localMaven   = "Maven" at "file://"+Path.userHome+"/.m2/repository"
-  val localIvy     = "Ivy" at "file://"+Path.userHome+"/.ivy2/local"
-  
-  val pluginRepo = "Pyrostream Repository" at "http://siasia.insomnia247.nl/repo-snapshots/"
-  val github     = "net.ps" % "github-plugin" % "1.0"
-}
