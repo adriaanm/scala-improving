dd32f3d2ad936fc6be8b5288c756f7594c2ff6f8
Paul Phillips <paulp@improving.org>
2010-11-18 14:29:07 -0800

    Fiddling around trying to make sonatype happy.

--- project/build/ImprovingProject.scala
+++ project/build/ImprovingProject.scala
@@ -1,3 +1,9 @@
+/** Improving: An unconstrained collection of scala code.
+ *  Copyright 2005-2010 Paul Phillips
+ * 
+ *  Distributed under the "Simplified BSD License" in LICENSE.txt.
+ */
+
 import sbt._
 
 class ImprovingProject(info: ProjectInfo)
@@ -5,8 +11,9 @@ class ImprovingProject(info: ProjectInfo)
            with ImprovingPublish {
 
   // repos
-  val localMaven    = "Maven" at "file://"+Path.userHome+"/.m2/repository"
-  val localIvy      = "Ivy" at "file://"+Path.userHome+"/.ivy2/local"
+  val localMaven = "Maven" at "file://"+Path.userHome+"/.m2/repository"
+  val localIvy   = "Ivy" at "file://"+Path.userHome+"/.ivy2/local"
+  val sonatype   = "Sonatype" at "https://oss.sonatype.org/content/groups/public"
 
   // local use
   override def localScala = System.getenv("scala.local") match {
